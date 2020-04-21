dbe4b7d7423e804bbaa10b9e8b7391ff062b733f
Amazer <149779511@qq.com>
2020-04-21 11:31:50 +0800

    提交第一版

---
+++ NetClient/Assets/Scripts/Echo.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net.Sockets;
+
+public class Echo : MonoBehaviour
+{
+
+    Socket socket;
+    public InputField inputField;
+    public Text text;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Connection()
+    {
+        socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        socket.Connect("127.0.0.1", 8888);
+    }
+    public void Send()
+    {
+        string sendStr = inputField.text;
+        inputField.text = "";
+        byte[] sendBytes = System.Text.Encoding.Default.GetBytes(sendStr);
+        socket.Send(sendBytes);
+
+        byte[] readBuffer = new byte[1024];
+        int count = socket.Receive(readBuffer);
+        string recvStr = System.Text.Encoding.Default.GetString(readBuffer, 0, count);
+        text.text = recvStr;
+        socket.Close();
+    }
+}
---
+++ NetServer/NetServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace NetServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            SocketFunc();
+            Console.Read();
+        }
+        static void SocketFunc()
+        {
+            Socket listenfd = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPAddress ipAdr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEp = new IPEndPoint(ipAdr, 8888);
+            listenfd.Bind(ipEp);
+
+            listenfd.Listen(0);
+            Console.WriteLine("【服务器】启动成功！");
+
+            while(true)
+            {
+                Socket connfd = listenfd.Accept();
+                Console.WriteLine("【服务器】Accetp！");
+
+                byte[] readBuff = new byte[1024];
+                int count = connfd.Receive(readBuff);
+                string recvStr = System.Text.Encoding.Default.GetString(readBuff, 0, count);
+                Console.WriteLine("【服务器接收】"+recvStr);
+
+                byte[] sendBytes = System.Text.Encoding.Default.GetBytes(recvStr);
+                connfd.Send(sendBytes);
+            }
+        }
+    }
+}
